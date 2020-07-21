80504d5cc24087ce07cb29bfd8620c063655ae86
Kristofer Björnson <dafer45@hotmail.com>
2020-07-21 10:57:17 +0200

    Changed draw.asm to gameLogic.asm.

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ yasm -gdwarf2 -felf64 src/printString.asm -o build/printString.o
 yasm -gdwarf2 -felf64 src/sleep.asm -o build/sleep.o
 yasm -gdwarf2 -felf64 src/input.asm -o build/input.o
 yasm -gdwarf2 -felf64 src/clearScreen.asm -o build/clearScreen.o
-yasm -gdwarf2 -felf64 src/draw.asm -o build/draw.o
+yasm -gdwarf2 -felf64 src/gameLogic.asm -o build/gameLogic.o
 yasm -gdwarf2 -felf64 src/random.asm -o build/random.o
 yasm -gdwarf2 -felf64 src/intToString.asm -o build/intToString.o
 
